301b3b47fe4897143fcc18359cf331c718d798dd
aquajr <ayandaobosi@gmail.com>
2024-06-01 10:53:36 +0300

    Dev: Update app for deployment

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+        return f'https://www.gravatar.com/avatar/{digest}?d=retro&s={size}'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
